7ac04d53d225fb7df7ec8dc6e9af348fd6b8c71b
ET2319013 <168468071+ET2319013@users.noreply.github.com>
2024-05-17 20:02:34 +0300

    +

--- migration.sql
+++ migration.sql
@@ -1,7 +1,5 @@
-create table users(
-	id serial primary key,
-	email varchar(64) not null,
-	password varchar(72) not null
-);
+create table  tbl_user (id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(50), password VARCHAR(50));
+
+create table tbl_posts (id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(50), title VARCHAR(50), post VARCHAR(1024));
 
 SELECT 'down SQL query';
